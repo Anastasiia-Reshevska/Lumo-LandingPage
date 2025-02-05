2282917cee719f09d6f5d7826d7c39d00afbc9a6
Anastasiia-Rechevska <volokh.qs@gmail.com>
2025-02-05 21:45:46 +0200

    started creating a discount section.

---
+++ resources/views/components/discount/index.blade.php
@@ -0,0 +1,12 @@
+<section class="discount container">
+  <div class="discount__wrapper">
+    <p class="discount__subtitle">DON'T MISS OUT ON EXCLUSIVE SAVINGS!</p>
+  <p class="discount__title"><span>35%</span> DISCOUNT ON LAUNCH DAY</p>
+  <button class="discount__btn-additional btn-primary btn-blue-center">BOOK YOUR SEAT
+    <div class="arrow arrow--small" aria-hidden="true">
+        <div class="arrow__direction arrow_white"></div>
+    </div>
+  </button>
+  </div>
+  
+</section>
